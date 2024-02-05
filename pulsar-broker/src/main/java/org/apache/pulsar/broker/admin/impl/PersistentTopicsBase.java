3e3efe694aaeb483ab3a7ebc1493f3b574a8de4b
Jiwe Guo <technoboy@apache.org>
2024-02-05 16:30:04 +0800

    [fix][broker] Fix String wrong format (#21829)
    (cherry picked from commit fd3d8b9298b13c82bc2bca2149211a865b41ee88)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PersistentTopicsBase.java
@@ -2783,7 +2783,7 @@ public class PersistentTopicsBase extends AdminResource {
 
                     @Override
                     public String toString() {
-                        return String.format("Topic [{}] get entry batch size",
+                        return String.format("Topic [%s] get entry batch size",
                                 PersistentTopicsBase.this.topicName);
                     }
                 }, null);
@@ -2890,7 +2890,7 @@ public class PersistentTopicsBase extends AdminResource {
 
                         @Override
                         public String toString() {
-                            return String.format("Topic [{}] internal get message by id",
+                            return String.format("Topic [%s] internal get message by id",
                                     PersistentTopicsBase.this.topicName);
                         }
                     }, null);
@@ -3102,7 +3102,7 @@ public class PersistentTopicsBase extends AdminResource {
 
                             @Override
                             public String toString() {
-                                return String.format("Topic [{}] internal examine message async",
+                                return String.format("Topic [%s] internal examine message async",
                                         PersistentTopicsBase.this.topicName);
                             }
                         }, null);
