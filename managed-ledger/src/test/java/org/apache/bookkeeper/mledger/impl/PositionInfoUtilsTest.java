08cbee78dfe6b7783c12b12d0c34e69ab1e7d35c
Andrey Yegorov <andrey.yegorov@datastax.com>
2024-05-16 15:52:36 -0700

    fix

--- managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/PositionInfoUtilsTest.java
+++ managed-ledger/src/test/java/org/apache/bookkeeper/mledger/impl/PositionInfoUtilsTest.java
@@ -29,9 +29,13 @@ import java.util.List;
 import org.apache.bookkeeper.mledger.proto.LightMLDataFormats;
 import org.apache.bookkeeper.mledger.proto.MLDataFormats;
 import org.apache.pulsar.common.allocator.PulsarByteBufAllocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class PositionInfoUtilsTest {
+    private static final Logger log = LoggerFactory.getLogger(PositionInfoUtilsTest.class);
+
     @Test
     public void testSerializeDeserialize() throws Exception {
         PositionImpl position = new PositionImpl(1, 2);
@@ -100,9 +104,7 @@ public class PositionInfoUtilsTest {
         ManagedCursorImpl.MarkDeleteEntry entry = new ManagedCursorImpl.MarkDeleteEntry(position,
                 Map.of("foo", 1L), null, null);
 
-        // passes with 31
-        // fails with >= 32
-        final int numRanges = 32;
+        final int numRanges = 10000;
         ByteBuf result = PositionInfoUtils.serializePositionInfo(entry, position, (scanner) -> {
             for (int i = 0; i < numRanges; i++) {
                 scanner.acceptRange(i*4 + 1, i*4 + 2, i*4 + 3, i*4 + 4);
@@ -120,19 +122,63 @@ public class PositionInfoUtilsTest {
         lighPositionInfoParsed.parseFrom(result, result.readableBytes());
         result.readerIndex(idx);
 
+        validateLightproto(lighPositionInfoParsed, numRanges);
+
         // serialize lightproto
         int serializedSz = lighPositionInfoParsed.getSerializedSize();
         ByteBuf lightResult = PulsarByteBufAllocator.DEFAULT.buffer(serializedSz);
         lighPositionInfoParsed.writeTo(lightResult);
 
+        byte[] light = ByteBufUtil.getBytes(lightResult);
+        byte[] util = ByteBufUtil.getBytes(result);
+
+        assertEquals(light.length, util.length);
+
+        for (int i = 0; i < light.length; i++) {
+            if (light[i] != util[i]) {
+                log.error("Mismatch at index {} light={} util={}", i, light[i], util[i]);
+            }
+        }
+
+        assertEquals(light, util);
+
         // deserialize lightproto -> protobuf
-        parseProtobufAndValidate(ByteBufUtil.getBytes(lightResult), numRanges);
+        parseProtobufAndValidate(light, numRanges);
 
         // deserialize PIUtils -> protobuf
-        parseProtobufAndValidate(ByteBufUtil.getBytes(result), numRanges);
+        parseProtobufAndValidate(util, numRanges);
 
         result.release();
         lightResult.release();
+    }
+
+    private static void validateLightproto(LightMLDataFormats.PositionInfo lighPositionInfoParsed, int numRanges) {
+        assertEquals(1, lighPositionInfoParsed.getLedgerId());
+        assertEquals(2, lighPositionInfoParsed.getEntryId());
+
+        assertEquals(1, lighPositionInfoParsed.getPropertiesCount());
+        assertEquals("foo", lighPositionInfoParsed.getPropertyAt(0).getName());
+        assertEquals(1, lighPositionInfoParsed.getPropertyAt(0).getValue());
+
+        assertEquals(numRanges, lighPositionInfoParsed.getIndividualDeletedMessagesCount());
+        int curr = 0;
+        for (int i = 0; i < numRanges; i++) {
+            assertEquals(i*4 + 1, lighPositionInfoParsed.getIndividualDeletedMessageAt(curr).getLowerEndpoint().getLedgerId());
+            assertEquals(i*4 + 2, lighPositionInfoParsed.getIndividualDeletedMessageAt(curr).getLowerEndpoint().getEntryId());
+            assertEquals(i*4 + 3, lighPositionInfoParsed.getIndividualDeletedMessageAt(curr).getUpperEndpoint().getLedgerId());
+            assertEquals(i*4 + 4, lighPositionInfoParsed.getIndividualDeletedMessageAt(curr).getUpperEndpoint().getEntryId());
+            curr++;
+        }
+
+        assertEquals(numRanges, lighPositionInfoParsed.getBatchedEntryDeletionIndexInfosCount());
+        curr = 0;
+        for (int i = 0; i < numRanges; i++) {
+            assertEquals(i*2 + 1, lighPositionInfoParsed.getBatchedEntryDeletionIndexInfoAt(curr).getPosition().getLedgerId());
+            assertEquals(i*2 + 2, lighPositionInfoParsed.getBatchedEntryDeletionIndexInfoAt(curr).getPosition().getEntryId());
+            assertEquals(7L, lighPositionInfoParsed.getBatchedEntryDeletionIndexInfoAt(curr).getDeleteSetAt(0));
+            assertEquals(8L, lighPositionInfoParsed.getBatchedEntryDeletionIndexInfoAt(curr).getDeleteSetAt(1));
+            curr++;
+        }
     }
 
     private static void parseProtobufAndValidate(byte[] data, int numRanges) throws InvalidProtocolBufferException {
