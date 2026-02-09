0791790702cb7deb63f7abcb61e2cf10ea2ce82c
Hao Zhang <zhanghao1@cmss.chinamobile.com>
2026-02-09 19:15:58 +0800

    [fix][broker] Fix transactionMetadataFuture completeExceptionally with null value (#25231)
    Co-authored-by: 张浩 <zhanghao60@100.me>
    (cherry picked from commit 0e5d4246f3abc7235fd2547899742ecd85d4d593)
    (cherry picked from commit 2b85b8752b5ae00fb197e19ccd0476335e5e860a)

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/TransactionsBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/TransactionsBase.java
@@ -259,7 +259,7 @@ public abstract class TransactionsBase extends AdminResource {
 
             FutureUtil.waitForAll(producedPartitionsFutures).whenComplete((x, t) -> {
                 if (t != null) {
-                    transactionMetadataFuture.completeExceptionally(e);
+                    transactionMetadataFuture.completeExceptionally(t);
                     return;
                 }
 
