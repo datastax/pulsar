4608cd1577ac00bb4e2bb7b507eb7a04005ef5cd
priyanshu-ctds <ext-priyanshu.pareta@datastax.com>
2026-04-06 17:24:24 +0530

    Fix checkstyle issue

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/sinks/KinesisSinkTester.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/io/sinks/KinesisSinkTester.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import lombok.AllArgsConstructor;
 import lombok.Cleanup;
 import lombok.Data;
