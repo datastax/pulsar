4e67f4467c17641a3be8e75e9764d6f9068862c1
hshankar31 <ext-hariharan.shankar@datastax.com>
2026-07-21 10:25:58 +0530

    alpine-will-fail-with-classname

--- tests/integration/src/test/java/org/apache/pulsar/tests/integration/cli/HealthCheckTest.java
+++ tests/integration/src/test/java/org/apache/pulsar/tests/integration/cli/HealthCheckTest.java
@@ -95,7 +95,7 @@ public class HealthCheckTest extends TestRetrySupport {
 
     @Test
     public void testZooKeeperDown() throws Exception {
-        pulsarCluster.getZooKeeper().execCmd("pkill", "-STOP", "-f", "QuorumPeerMain");
+        pulsarCluster.getZooKeeper().execCmd("pkill", "-STOP", "java");
         assertHealthcheckFailure();
     }
 
@@ -111,7 +111,7 @@ public class HealthCheckTest extends TestRetrySupport {
     @Test
     public void testBookKeeperDown() throws Exception {
         for (BKContainer b : pulsarCluster.getBookies()) {
-            b.execCmd("pkill", "-STOP", "-f", "Main");
+            b.execCmd("pkill", "-STOP", "java");
         }
         assertHealthcheckFailure();
     }
